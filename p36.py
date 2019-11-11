886df9acb2807d40fa01ec4185782a2c3fbe6714
afarica <afaricanistan>
2019-11-12 04:47:01 +0600

    task2.16

---
+++ p36.py
@@ -0,0 +1,12 @@
+a=input("Enter your word:")
+if a=='Hello':
+	print(
+		"google_kazakstan.txt" /n
+		"google_paris.txt" /n
+		"google_uar.txt" /n
+		"google_kyrgystan.txt" /n
+		"google_san_francisco.txt" /n
+		"google_germany.txt" /n
+		"google_moscow.txt" /n
+		"google_sweden.txt")
+
